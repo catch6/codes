4c504edee4e62d960029d2e92b5a60ae28f131d4
Catch <catchlife6@163.com>
2022-12-04 13:27:02 +0800

    cglib 动态代理

--- design-pattern/src/main/java/net/wenzuo/codes/designpattern/proxy/bydynamic/bycglib/Main.java
+++ design-pattern/src/main/java/net/wenzuo/codes/designpattern/proxy/bydynamic/bycglib/Main.java
@@ -1,6 +1,17 @@
 package net.wenzuo.codes.designpattern.proxy.bydynamic.bycglib;
 
 /**
+ * 使用 cglib 动态代理需要引入下面依赖
+ * <pre>
+ * {@code
+ * <dependency>
+ *     <groupId>cglib</groupId>
+ *     <artifactId>cglib</artifactId>
+ *     <version>3.3.0</version>
+ * </dependency>
+ * }
+ * </pre>
+ *
  * @author Catch
  * @since 2022-12-04
  */
